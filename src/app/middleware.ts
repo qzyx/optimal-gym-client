c3756a3d72507e19cd9e824a8af4942c2fe58f4a
Martin Lazebnik <martin.lazebnik3@gmail.com>
2025-04-14 12:04:39 +0200

    Working on middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -2,12 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
   const session = req.cookies.get("appwrite-session")?.value;
-
+  const url = req.nextUrl.clone();
+  if (session && url.pathname === "/login") {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
   if (!session) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
   return NextResponse.next();
 }
 export const config = {
-  matcher: ["/dashboard/:path*"], // Apply middleware to these paths
+  matcher: ["/dashboard/:path*", "/login", "/register"],
 };
